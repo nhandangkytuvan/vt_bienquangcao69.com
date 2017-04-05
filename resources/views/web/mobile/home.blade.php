68dd591494425d6f3c0faebba7858fceaf334c11
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-04-05 14:19:36 +0700

    oke

--- resources/views/web/mobile/home.blade.php
+++ resources/views/web/mobile/home.blade.php
@@ -14,60 +14,65 @@
 	<main class="container home">
 		<h3>Sản phẩm gần đây</h3>
 		<div class="posts-sp">
+			@if($data['posts_sp'][0])
 			<div class="post-top">
-				<h3 class="post-avatar"><a href="#"><img src="{{ asset('public/images/mobile/home-1.png') }}" class="img-responsive center-block"></a></h3>
-				<h3 class="post-name"><a href="">SP-Biển quảng cáo cửa hiệu</a></h3>
-				<p class="text-justify post-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo laborum libero rem hic, sint, rerum facilis qui ad maiores ab quia autem.</p>
+				<h3 class="post-avatar">
+					<a href="{{ MyAPI::getUrlPost($data['posts_sp'][0]->id) }}">
+						<img src="{{ asset('public/img/'.$data['posts_sp'][0]->post_avatar) }}" class="img-responsive center-block">
+					</a>
+				</h3>
+				<h3 class="post-name"><a href="{{ MyAPI::getUrlPost($data['posts_sp'][0]->id) }}">{{ $data['posts_sp'][0]->post_name }}</a></h3>
+				<p class="text-justify post-description">
+					{{ MyAPI::limitWord($data['posts_sp'][0]->post_description,22) }}
+				</p>
 			</div>
+			@php unset($data['posts_sp'][0]); @endphp
+			@endif
 			<div class="post-middle">
-				<div class="flex flex1 flex-wrap-wrap justify-content-around">
+				<div class="flex flex1 flex-wrap-wrap">
+					@foreach($data['posts_sp'] as $post)
 					<div class="flex1col1">
-						<h3><a href="#"><img src="{{ asset('public/images/mobile/sp-1.png') }}" class="center-block img-responsive"></a></h3>
-						<h4><a href="">Lorem ipsum dolor sit amet, consectetur.</a></h4>
+						<h3>
+							<a href="{{ MyAPI::getUrlPost($post->id) }}">
+								<img src="{{ asset('public/img/'.$post->post_avatar) }}" class="center-block img-responsive">
+							</a>
+						</h3>
+						<h4><a href="">{{ $post->post_name }}</a></h4>
 					</div>
-					<div class="flex1col1">
-						<h3><a href="#"><img src="{{ asset('public/images/mobile/sp-2.png') }}" class="center-block img-responsive"></a></h3>
-						<h4><a href="">Lorem ipsum dolor sit amet, consectetur.</a></h4>
-					</div>
-					<div class="flex1col1">
-						<h3><a href="#"><img src="{{ asset('public/images/mobile/sp-3.png') }}" class="center-block img-responsive"></a></h3>
-						<h4><a href="">Lorem ipsum dolor sit amet, consectetur.</a></h4>
-					</div>
-					<div class="flex1col1">
-						<h3><a href="#"><img src="{{ asset('public/images/mobile/sp-4.png') }}" class="center-block img-responsive"></a></h3>
-						<h4><a href="">Lorem ipsum dolor sit amet, consectetur.</a></h4>
-					</div>
-					<div class="flex1col1">
-						<h3><a href="#"><img src="{{ asset('public/images/mobile/sp-5.png') }}" class="center-block img-responsive"></a></h3>
-						<h4><a href="">Lorem ipsum dolor sit amet, consectetur.</a></h4>
-					</div>
-					<div class="flex1col1">
-						<h3><a href="#"><img src="{{ asset('public/images/mobile/sp-6.png') }}" class="center-block img-responsive"></a></h3>
-						<h4><a href="">Lorem ipsum dolor sit amet, consectetur.</a></h4>
-					</div>
+					@endforeach
 				</div>
 			</div>
 		</div>
 		<h3>Tin tức - Sự kiện</h3>
 		<div class="posts-new">
+			@if($data['posts_new'][0])
 			<div class="post-top">
 				<div class="flex flex1 justify-content-between">
 					<div class="flex1col1">
-						<h3 class="post-avatar"><a href="#"><img src="{{ asset('public/images/mobile/new-1.jpg') }}" class="center-block img-responsive"></a></h3>
+						<h3 class="post-avatar">
+							<a href="{{ MyAPI::getUrlPost($data['posts_new'][0]->id) }}">
+								<img src="{{ asset('public/img/'.$data['posts_new'][0]->post_avatar) }}" class="center-block img-responsive">
+							</a>
+						</h3>
 					</div>
 					<div class="flex1col2">
-						<h3 class="post-name"><a href="#">BIỂN QUẢNG CÁO 69.COM</a></h3>
-						<p class="post-description">Bạn cũng nên dành nhiều thời gian để đưa ra cách thiết kế nội thất cửa hàng sao cho phù hợp với diện tích và phong cách của loại hình sản phẩm bạn sẽ kinh doanh. Bạn cần tìm công...</p>
+						<h3 class="post-name"><a href="{{ MyAPI::getUrlPost($data['posts_new'][0]->id) }}">{{ $data['posts_new'][0]->post_name }}</a></h3>
+						<p class="post-description">{{ MyAPI::limitWord($data['posts_new'][0]->post_description,30) }}...</p>
 					</div>
 				</div>
 			</div>
+			@php unset($data['posts_new'][0]); @endphp
+			@endif
 			<div class="post-middle">
 				<ul>
-					<li><h4><i class="fa fa-angle-double-right"></i><a href="">Lorem ipsum dolor sit amet, consectetur.</a></h4></li>
-					<li><h4><i class="fa fa-angle-double-right"></i><a href="">Lorem ipsum dolor sit amet, consectetur.</a></h4></li>
-					<li><h4><i class="fa fa-angle-double-right"></i><a href="">Lorem ipsum dolor sit amet, consectetur.</a></h4></li>
-					<li><h4><i class="fa fa-angle-double-right"></i><a href="">Lorem ipsum dolor sit amet, consectetur.</a></h4></li>
-					<li><h4><i class="fa fa-angle-double-right"></i><a href="">Lorem ipsum dolor sit amet, consectetur.</a></h4></li>
+					@foreach($data['posts_new'] as $post)
+					<li>
+						<h4>
+							<i class="fa fa-angle-double-right"></i>
+							<a href="{{ MyAPI::getUrlPost($post->id) }}">{{ $post->post_name }}</a>
+						</h4>
+					</li>
+					@endforeach
 				</ul>
 			</div>
 		</div>
