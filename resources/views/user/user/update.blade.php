f40d58b53691a2e34551de2ccd23b1343108f017
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-04-13 10:38:23 +0700

    oke

--- resources/views/user/user/update.blade.php
+++ resources/views/user/user/update.blade.php
@@ -3,6 +3,9 @@
     @include('menu.menuUser')
 @endsection('menu')
 @section('content')
+<div class="alert alert-success">
+    Để tránh bị lỗi , tạm thời bài viết về sản phẩm hãy vào mục <span class="label label-success"><i class="fa fa-shopping-basket"></i> viết sản phẩm </span> và chọn danh mục sản phẩm
+</div>
 <div class="panel panel-default">
     <div class="panel-heading text-center">
         Nội dung hoàn thiện
@@ -22,6 +25,7 @@
                     <li class="list-group-item">Số điện thoại mobile xuống dưới</li>
                     <li class="list-group-item">Bỏ border ở mobile</li>
                     <li class="list-group-item">Menu mobile border chuyển sang màu trắng</li>
+                    <li class="list-group-item">Fix error ảnh bài viết trong mobile</li>
                 </ul>
             </td>
             <td></td>
