988309eb17ac61d2b69aec00f31bd281a50ba7e5
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-04-13 17:30:54 +0700

    oke

--- resources/views/web/desktop/home.blade.php
+++ resources/views/web/desktop/home.blade.php
@@ -54,7 +54,9 @@
 					@endforeach
 				</div>
 				<div class="flex1col3">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.399613054525!2d105.74439961548623!3d21.016690686004868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3134537d5284fa77%3A0x1a7bc8a0647e5846!2zNzAgVMOieSBN4buXLCBU4burIExpw6ptLCBIw6AgTuG7mWksIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1491270430116" width="339" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+					<video width="100%" height="275px" controls>
+					  	<source src="{{ asset('public/video/videoled.mp4') }}" type="video/mp4">
+					</video>
 				</div>
 			</div>
 		</div>
